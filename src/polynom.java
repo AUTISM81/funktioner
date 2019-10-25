0046abe6ef660d38a8b1c536f23c9419d8c47719
AUTISM81 <christian.backstrom@elev.ga.ntig.se>
2019-10-25 11:18:08 +0200

    polynom funktioner lektion 25/10-19

--- src/polynom.java
+++ src/polynom.java
@@ -1,7 +1,7 @@
 
 import javax.swing.*;
 
-public class polynom {
+/*public class polynom {
     public static void main(String [] args){
         String a = JOptionPane.showInputDialog(null, "we are going to calculate ax + b = 0" + "\nplease enter variable a");
                 double a1 = Integer.parseInt(a);
@@ -11,9 +11,22 @@ public class polynom {
         JOptionPane.showMessageDialog(null, "your x vairable is " + x + " in your " + a + "x" + " + " + b);
     }
 }
-
-public class polynom2 {
+*/
+public class polynom {
     public static void main(String [] args){
-        
+        String a = JOptionPane.showInputDialog(null, "we are going to calculate ax*x + bx + c = 0" + "\nplease enter variable a");
+            double a1 = Integer.parseInt(a);
+        String b = JOptionPane.showInputDialog(null, "please enter variable b");
+            double b1 = Integer.parseInt(b);
+        String c = JOptionPane.showInputDialog(null, "please enter variable c");
+            double c1 = Integer.parseInt(c);
+                    //koden
+        double sqrt = Math.sqrt((b1*b1) - (4*a1*c1));
+        double x1 = ((-1*b1) + sqrt)/2*a1;
+        double x2 = ((-1*b1) - sqrt)/2*a1;
+        if (double.isNaN(x1))
+
+        JOptionPane.showMessageDialog(null, "your x vairable is " + x1 + " or " + x2 + " when " + a1 + "x*x" + " + " + b1 + "x + " + c + " = 0");
+
     }
 }
